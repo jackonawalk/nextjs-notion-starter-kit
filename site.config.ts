5edc3eab28df464913bc1a5f6c39110b61d03475
Jack Sellwood <jacksellwood@me.com>
2023-11-24 15:35:02 -0600

    customize and update

--- site.config.ts
+++ site.config.ts
@@ -2,8 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
-
+  rootNotionPageId: 'cfa674b25867440ca332eebe9db45903',
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
